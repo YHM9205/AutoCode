6f8bf2fa12d2e40204b3d6f214f5ad6f176299db
Yousif <lunatic.9205@icloud.com>
2026-09-05 13:51:51 +0300

    editing home page route and import the obdCode

--- server.js
+++ server.js
@@ -3,45 +3,54 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const ObdCode = require('./models/ObdCode')
-const app = express() // creates a express application
-const PORT = 3000
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
 const myCars = [
-    { model: 'Ford F-150', year: 2012, type: 'Pickup'},
-    { model: 'Ford Taurus', year: 2012, type: 'Sedan'},
-    { model: 'Mazda CX-9', year: 2018, type: 'SUV'}
+    { model: 'Ford F-150', year: 2012, type: 'Pickup' },
+    { model: 'Ford Taurus', year: 2012, type: 'Sedan' },
+    { model: 'Mazda CX-9', year: 2018, type: 'SUV' }
 ]
-app.set('view engine','ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
-
-
-
-
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.once('open', () => {
+    console.log('Connected')
+});
 
 
 // Routes go here
-app.get('/',(req,res)=>{
-    res.render('index')
-})
+app.get('/', async (req, res) => {
+    try{
+        const codes = await ObdCode.find({})
+    res.render('index.ejs',{ codes, myCars })
+} catch (Error) {
+    console.log(Error)
+    res.send(Error)
+}
+});
 
 
 
-app.get('/system',(req,res)=>{
+app.get('/system', (req, res) => {
     const systemStatus = [
-        { name: 'Engine Diagnostics', status: 'Optimal', code: 'P0000'},
-        { name: 'Transmission Gearbox', status: 'need to change the clutches', code: 'P0920'},
-        { name: 'Cooling System', status: 'stable', code: 'P0119'}
+        { name: 'Engine Diagnostics', status: 'Optimal', code: 'P0000' },
+        { name: 'Transmission Gearbox', status: 'need to change the clutches', code: 'P0920' },
+        { name: 'Cooling System', status: 'stable', code: 'P0119' }
     ]
-    res.render('system', {system: systemStatus})
-})
- 
-app.get ('/garage',(req,res)=>{
-    res.render('garage',{cars: myCars})
+    res.render('system', { system: systemStatus })
 })
 
+app.get('/garage', (req, res) => {
+    res.render('garage', { cars: myCars })
+})
 
 
-app.listen(3000,()=>{
+
+app.listen(3000, () => {
     console.log('App is Running')
 }) // listen on port 3000
